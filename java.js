3bbc4bf2aa2dd6fe556426950158367ccd33d9b3
daniben94 <160612871+daniben94@users.noreply.github.com>
2024-03-01 13:40:56 -0300

    java.js

--- java.js
+++ java.js
@@ -1,50 +1,67 @@
-body {
-  font-family: Arial, sans-serif;
+console.log("Hola Mundo");
+
+let intentos = 6;
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH'];
+let palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+
+window.addEventListener('load', init);
+
+function init() {
+  const button = document.getElementById("guess-button");
+  button.addEventListener("click", intentar);
 }
 
-h1 {
-  text-align: center;
-  margin-top: 50px;
+function leerIntento() {
+  let intento = document.getElementById("guess-input").value;
+  intento = intento.toUpperCase();
+  return intento;
 }
 
-#guess-input {
-  display: block;
-  margin: 0 auto 20px;
-  padding: 10px;
-  font-size: 18px;
-  width: 250px;
-  border: 1px solid #ccc;
+function intentar() {
+  const INTENTO = leerIntento();
+
+  if (INTENTO === palabra) {
+    terminar("<h1>¡GANASTE APENAMI! 😀</h1>");
+    return;
+  }
+
+  const GRID = document.getElementById("grid");
+  const ROW = document.createElement('div');
+  ROW.className = 'row';
+
+  for (let i in palabra) {
+    const SPAN = document.createElement('span');
+    SPAN.className = 'letter';
+
+    if (INTENTO[i] === palabra[i]) {
+      SPAN.innerHTML = INTENTO[i];
+      SPAN.style.backgroundColor = '#79b851'; // Verde
+    } else if (palabra.includes(INTENTO[i])) {
+      SPAN.innerHTML = INTENTO[i];
+      SPAN.style.backgroundColor = '#f3c237'; // Amarillo
+    } else {
+      SPAN.innerHTML = INTENTO[i];
+      SPAN.style.backgroundColor = '#a4aec4'; // Gris
+    }
+
+    ROW.appendChild(SPAN);
+  }
+
+  GRID.appendChild(ROW);
+  intentos--;
+
+  if (intentos === 0) {
+    terminar("<h1>¡LA PROXIMA MI REY! 😖</h1>");
+  }
 }
 
-#guess-button {
-  display: block;
-  margin: 0 auto;
-  padding: 10px 20px;
-  font-size: 18px;
-  background-color: #0CBABA;
-  color: #fff;
-  border: none;
-  border-radius: 3px;
-}
+function terminar(mensaje) {
+  const INPUT = document.getElementById("guess-input");
+  const BOTON = document.getElementById("guess-button");
 
-button:active {
-  transform: scale(0.95);
-}
+  INPUT.disabled = true;
+  BOTON.disabled = true;
 
-.letter {
-  border: 1px solid black;
-  padding: 3px;
-  margin: 3px;
-  min-width: 20px;
-  text-align: center;
-  display: inline-block;
+  const contenedor = document.getElementById('guesses');
+  contenedor.innerHTML = mensaje;
 }
-
-.row {
-  margin-bottom: 10px;
-}
-
-.input-area {
-  margin: 30px;
-}
-
